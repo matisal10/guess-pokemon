37062b646a2619b358061775510d5ad8b118ecd0
MatiSal <salinasmatias100@gmail.com>
2023-06-01 16:35:32 -0300

    add table with counts and store data in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import api, { POKEMON } from "./api"
+import api from "./api"
 import { Pokemon } from "./types";
 
 import './index.css'
@@ -10,11 +10,21 @@ function App() {
   const [name, setName] = useState('')
   const [flag, setFlag] = useState<boolean>(true)
   const [flagReset, setFlagReset] = useState(false)
+  const initialHists = () => Number(localStorage.getItem('hits'));
+  const initialErrors = () => Number(localStorage.getItem('errors'));
+  const [hits, setHits] = useState(initialHists)
+  const [errors, setErrors] = useState(initialErrors)
 
   const getPokemon = async () => {
     const randomPokemon = await api.random();
     setPokemon(randomPokemon);
+    console.log(randomPokemon)
   }
+
+  useEffect(() => {
+    localStorage.setItem('hits', hits.toString())
+    localStorage.setItem('errors', errors.toString())
+  }, [hits, errors])
 
   useEffect(() => {
     getPokemon()
@@ -30,16 +40,25 @@ function App() {
       }
       setFlagReset(true)
       setFlag(true)
+      setHits(hits + 1)
     }
     else {
       setFlag(false)
+      setErrors(errors + 1)
     }
   }
 
-  const handleChange = (e: any) => {
+  const handleChange = (e:any) => {
     const modifiedString = e.currentTarget.value.replace(".", "");
     setName(modifiedString.toLowerCase().replace(/\s/g, ''))
   }
+
+  const handleKeyPress = (e:any) => {
+    if (e.key === 'Enter') {
+      validatorText();
+    }
+  };
+  
 
   const reset = () => {
     const img = document.getElementById('img')
@@ -57,12 +76,26 @@ function App() {
 
   return (
     <main >
-
+      <div className="nes-table-responsive container-table">
+        <table className="nes-table is-bordered ">
+          <thead>
+            <tr>
+              <th>Aciertos</th>
+              <th>Errores</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{hits}</td>
+              <td>{errors}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       {pokemon ?
         <div className="containerImg">
           {/* <span id="text" className="nes-text " style={{ display: "none" }}>{pokemon?.name}</span> */}
           <img id="img" src={pokemon.image} />
-
         </div>
         :
         <></>
@@ -78,7 +111,6 @@ function App() {
           <div className="nes-container is-rounded is-dark containerReset">
             <span className="nes-text ">Muy bien, Adivinaste!</span>
             <button type="button" className="nes-btn " onClick={reset}>Volver A Jugar</button>
-
           </div>
           :
           <div>
@@ -86,7 +118,8 @@ function App() {
             <div className="nes-field containerInput">
               <input type="text" id="name_field" className="nes-input"
                 value={name}
-                onChange={(e) => handleChange(e)} />
+                onChange={(e) => handleChange(e)}
+                onKeyDown={handleKeyPress} />
               <button type="button" className="nes-btn is-primary" onClick={validatorText}>Adivinar</button>
             </div>
           </div>
